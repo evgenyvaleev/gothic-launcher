c701e98488ddda61164c09f2071cd4ced8738fb2
Evgeny Valeev <evgenyvlv@gmail.com>
2024-09-09 18:56:55 +0300

    First commit

---
+++ GothicLauncher/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Diagnostics;
+using System.IO;
+
+namespace GothicLauncher
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var dir = Path.GetDirectoryName(Process.GetCurrentProcess().MainModule.FileName);
+
+            try
+            {
+                File.Copy(Path.Combine(dir, "_SystemPack.ini"), Path.Combine(dir, "SystemPack.ini"), true);
+            }
+            catch {}
+            
+            Process.Start(Path.Combine(dir, "GothicMod.exe"), string.Join(" ", args));
+        }
+    }
+}
